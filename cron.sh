e696320727bc0d81a3f5ba4182d1545da5e87139
JmeHsieh <jmehsieh@gmail.com>
2017-01-29 12:20:57 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 VENV_DIR="venv"
-SSH_KEY_FILE="${HOME}/.ssh/id_rsa"
 
 if [ ! -d "${VENV_DIR}" ]; then
   virtualenv "${VENV_DIR}"
@@ -13,13 +12,10 @@ fi
 eval "source ${VENV_DIR}/bin/activate"
 pip install -r requirements.txt
 
-# make sure no passphrase set on ${SSH_KEY_FILE}
-# install `keychain` otherwise.
-export GIT_SSH_COMMAND="ssh -i ${SSH_KEY_FILE}"
 python backup.py
 
 if [ "$?" -ne 0 ]; then
-  echo "failure: unable to generate repo info."
+  echo "failure: unable to backup hackfoldrs."
   exit 1
 fi
 eval "deactivate"
